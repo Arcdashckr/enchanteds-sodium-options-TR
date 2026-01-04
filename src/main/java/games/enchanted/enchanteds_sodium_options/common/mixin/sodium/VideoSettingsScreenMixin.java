43e18bdca79b79c0dce4e89614b0db1713fc2b5a
ioblackshaw <isaacoblackshaw@gmail.com>
2026-01-04 19:09:17 +0000

    better mixin target for custom options screen

--- src/main/java/games/enchanted/enchanteds_sodium_options/common/mixin/sodium/VideoSettingsScreenMixin.java
+++ src/main/java/games/enchanted/enchanteds_sodium_options/common/mixin/sodium/VideoSettingsScreenMixin.java
@@ -1,35 +1,33 @@
 package games.enchanted.enchanteds_sodium_options.common.mixin.sodium;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Cancellable;
 import games.enchanted.enchanteds_sodium_options.common.gui.VideoOptionsScreen;
-import games.enchanted.enchanteds_sodium_options.common.util.InputUtil;
+import net.caffeinemc.mods.sodium.client.gui.SodiumOptions;
 import net.caffeinemc.mods.sodium.client.gui.VideoSettingsScreen;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(VideoSettingsScreen.class)
+@Mixin(value = VideoSettingsScreen.class, priority = 950)
 public class VideoSettingsScreenMixin extends Screen {
-    @Shadow
-    @Final
-    private Screen prevScreen;
-
     protected VideoSettingsScreenMixin(Component title) {
         super(title);
     }
 
-    @Inject(
-        at = @At("TAIL"),
-        method = "init"
+    @WrapOperation(
+        at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/client/gui/SodiumOptions;isReadOnly()Z"),
+        method = "createScreen"
     )
-    private void afterScreenInit(CallbackInfo info) {
-        if(!InputUtil.shouldShowDebugWidgetBound()) {
-            Minecraft.getInstance().setScreen(new VideoOptionsScreen(prevScreen));
+    private static boolean wrapSodiumVideoScreenCtor(SodiumOptions instance, Operation<Boolean> original, Screen currentScreen, @Cancellable CallbackInfoReturnable<Screen> cir) {
+        boolean configReadOnly = original.call(instance);
+        if(!configReadOnly) {
+            cir.setReturnValue(new VideoOptionsScreen(currentScreen));
+            return false;
         }
+        return true;
     }
 }
